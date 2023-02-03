589d73b67cb5dbf5439c47854c423cd10160a0a7
Milan Kljajic <milankljajic04@gmail.com>
2023-02-03 13:15:20 +0100

    dodao dom

---
+++ dom.js
@@ -0,0 +1,22 @@
+//console.dir(document)
+//console.log(document.domain)
+
+//console.log(document.title);
+//document.title="moj prvi sajt"
+//console.log(document.all)
+//console.dir(document.head)
+//console.log(document.all[8])
+//document.all[8].textContent="Drugi naslov"
+console.log(document.forms);
+console.log(document.forms[0]);
+
+console.log(document.getElementsByClassName("list-item"));
+
+let items = document.getElementsByClassName("list-item");
+items[3].textContent = "promenio sam listu";
+items[2].style.backgroundcolor = "purple";
+
+for (let i = 0; i < items.length; i++) {
+  if (i % 2 == 0) items[i].style.backgroundColor = "grey";
+  else items[i].style.backgroundColor = "lightgrey";
+}
