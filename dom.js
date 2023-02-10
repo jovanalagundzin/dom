1c05cae0a455eaf2cec2412b4e0c07e9e5a67784
Milan Kljajic <milankljajic04@gmail.com>
2023-02-10 15:48:32 +0100

    dodali selektore

--- dom.js
+++ dom.js
@@ -20,3 +20,36 @@ for (let i = 0; i < items.length; i++) {
   if (i % 2 == 0) items[i].style.backgroundColor = "grey";
   else items[i].style.backgroundColor = "lightgrey";
 }
+
+console.dir(document.getElementsByClassId / "header-title");
+let headerTitle = document.getElementById("header-title");
+headerTitle.innerHTML = "<h3> ASD </h3>";
+console.log(headerTitle.textContent);
+console.log(headerTitle.innerText);
+
+//textcontent vs innertext
+
+console.log(document.getElementsByTagName("li"));
+let li = document.getElementsByTagName("li");
+li[1].textcontent = "asd";
+li[1].style.fontSize = "20px";
+li[1].style.fontWeight = "bold";
+
+let title = document.getElementsByClassName("title");
+title[1].textContent = "nesto";
+
+let container = document.getElementById("container");
+container.style.background = "purple";
+
+//Queryslector
+let drugilink = document.querySelector("list-item");
+console.log(drugilink);
+drugilink.style.backgroundColor = "red";
+
+let p = document.getElementById("sve jedno");
+
+//QueryselectaALL
+let lista = document.querySelectorAll(".list-item");
+console.log(lista);
+
+lista[2].textContent = "ne znam sta";
