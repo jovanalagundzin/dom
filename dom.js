b745a540b3d33336ea28f03fe95aa376b92ba778
Milan Kljajic <milankljajic04@gmail.com>
2023-02-24 16:03:35 +0100

    nesto

--- dom.js
+++ dom.js
@@ -7,6 +7,7 @@
 //console.dir(document.head)
 //console.log(document.all[8])
 //document.all[8].textContent="Drugi naslov"
+/*
 console.log(document.forms);
 console.log(document.forms[0]);
 
@@ -53,3 +54,47 @@ let lista = document.querySelectorAll(".list-item");
 console.log(lista);
 
 lista[2].textContent = "ne znam sta";
+*/
+//perentNode
+let listitem = document.getElementById("items");
+console.log(listitem.parentNode);
+
+//perentelement
+console.log(listitem.parentElement);
+listitem.parentElement.style.backgroundColor = "red";
+
+//childNodes
+console.log(listitem.childNodes);
+listitem.childNodes[1].style.backgroundColor = "purple";
+console.log(listitem.childNodes[0]);
+
+//children
+console.log(listitem.children);
+listitem.children[0].style.backgroundColor = "yellow";
+
+//firstchild
+console.log(listitem.firstChild); //console.log(listitem.childNodes[0])
+
+//firstElementchild
+console.log(listitem.firstElementChild); //console.log(listitem.children[0])
+//lastchild
+//
+console.log(listitem.lastChild);
+listitem.lastChild.style.backgroundColor = "blue";
+listitem.lastElementChild.style.backgroundColor="gery"
+
+//lastelementDhild (element)
+
+//sibling
+console.log(listitem.previousElementSibling)
+listitem.previousElementSibling.style.backgroundColor="blue"
+
+console.log(listitem.nextElementSibling)
+listitem.nextElementSibling.style.backgroundColor="red"
+
+let lista=document.querySelectorAll(.list-item)
+console.log(lista)
+lista[1].previousElementSibling.style.backgroundColor="blue"
+lista[1].nextElementSibling.style.backgroundColor="lightblue"
+
+lista[1].parentElement.parentElement.style.backgroundColor="orange"
